1e297fb184fcb3e8b833f053ab832d5094ed4f3c
Ayush Singh <80613660+AyushSingh16@users.noreply.github.com>
2025-02-17 19:04:40 +0530

    courseProgress schema added

--- database.js
+++ database.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.config.MONGODB_URL,{
-        userNewUrlParser:true,
-        useUnifiedTopology:true,
+  mongoose
+    .connect(process.config.MONGODB_URL, {
+      userNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => console.log("Database connected successfully..."))
     .catch((error) => {
-        console.log("Database connection failed!")
-        console.error(error);
-        process.exit(1);
-    })
+      console.log("Database connection failed!");
+      console.error(error);
+      process.exit(1);
+    });
 };
--- models/Profile.js
+++ models/Profile.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
-   gender: {
-    type:String,
-   },
-   dateOfBirth:{
-    type:String,
-   },
-   about:{
-    type:String,
-    trim:true,
-   },
-   contactNumber:{
-    type:Number,
-    trim:true,
-   },
-
-
-});    
+  gender: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: String,
+  },
+  about: {
+    type: String,
+    trim: true,
+  },
+  contactNumber: {
+    type: Number,
+    trim: true,
+  },
+});
 module.exports = mongoose.models("Profile", profileSchema);
