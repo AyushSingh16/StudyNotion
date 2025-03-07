7e4a38a4a801a8aea27b80eade7a079c4a79ca5e
Ayush Singh <80613660+AyushSingh16@users.noreply.github.com>
2025-03-07 16:13:50 +0530

    signUp functionality added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,72 +1,171 @@
 const User = require(".../models/User");
 const OTP = require("../models/OTP");
 const OTPGenerator = require("otp-generator");
-
+const bcrypt = require("bcrypt");
 
 //sendOTP
-exports.sendOTP = async(req,res)=>{
+exports.sendOTP = async (req, res) => {
+  try {
+    //fetch email from request's body
+    const { email } = req.body;
 
-    try{
+    //check if user already exists
+    const checkUserPresent = await User.findOne({ email });
 
-        //fetch email from request's body
-         const {email} = req.body;
-
-        //check if user already exists
-        const checkUserPresent = await User.findOne({email});
-
-        //if user already existed
-        if(checkUserPresent)
-        {
-            return res.status(401).json({
-                success:false,
-                message:"User Already Registered",
-            });
-        }
-
-        //generate OTP
-        var otp = OTPGenerator.generate(6,{
-            upperCaseAlphabets:false,
-            lowerCaseAlphabets:false,
-            specialChars:false,
-        });
-        console.log("OTP generated: ", otp);
-
-        // check unique otp or not
-        let result = otp.findOne({otp:otp});
-
-        while(result){
-            otp = OTPGenerator.generate(6,{
-                upperCaseAlphabets:false,
-                lowerCaseAlphabets:false,
-                specialChars:false,
-            });
-            result = otp.findOne({otp:otp});
-        }
-
-        const otpPayload = {email,otp};
-
-        //create an entry for otp in db
-        const otpBody = await OTP.create(otpPayload);
-        console.log(otpBody);
-
-        //return response successfull
-        res.status(200).json({
-            success:true,
-            message:"OTP sent successfully!",
-        })
-    } 
-    catch(error){
-        console.log(error);
-        return res.status(500).json({
-            success:false,
-            message:error.message,
-        })
+    //if user already existed
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User Already Registered",
+      });
     }
-    
-}
 
+    //generate OTP
+    var otp = OTPGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log("OTP generated: ", otp);
+
+    // check unique otp or not
+    let result = otp.findOne({ otp: otp });
+
+    while (result) {
+      otp = OTPGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+      result = otp.findOne({ otp: otp });
+    }
+
+    const otpPayload = { email, otp };
+
+    //create an entry for otp in db
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+
+    //return response successfull
+    res.status(200).json({
+      success: true,
+      message: "OTP sent successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //signUp
+exports.signUp = async (req, res) => {
+  try {
+    //data fetch from req  body
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      accountType,
+      contactNumber,
+      otp,
+    } = req.body;
 
+    //validating the data
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !contactNumber ||
+      !otp
+    ) {
+      return res.status(403).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    //match pass and conf. pass
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Password and ConfirmPassword values do not match!",
+      });
+    }
+
+    //check user already exists or not
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User is already registered",
+      });
+    }
+
+    //find most recent OTP for the user
+    const recentOTP = await OTP.find({ email })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    console.log(recentOTP);
+
+    //validate the otp
+    if (recentOTP.length == 0) {
+      //otp not found
+      return res.status(400).json({
+        success: false,
+        message: "OTP not found",
+      });
+    } else if (otp !== recentOTP.otp) {
+      //invalid otp
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+
+    //hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //entry creation in db
+
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contactNumber: null,
+    });
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      contactNumber,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/9.x/initials/svg?seed=${firstName} ${lastName}`,
+    });
+
+    //return res
+    return res.status(200).json({
+      success: true,
+      message: "User is registered successfully! ",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User cannot be registered! Please try again.",
+    });
+  }
+};
 
 //login
